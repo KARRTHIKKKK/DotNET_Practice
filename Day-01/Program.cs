cb47682611fc033448b1c2db052bb183c122126e
KARRTHIKKKK <karthikeyapepeti05@gmail.com>
2026-04-29 15:30:25 +0530

    added 1st code

--- Day-01/Program.cs
+++ Day-01/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Day_01
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to C# 12.0 and .NET 8.0!");
+        }
+    }
+}
