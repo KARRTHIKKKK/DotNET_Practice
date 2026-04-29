f125a4c4259ef05062e08acbf0bb9a585d113cbf
KARRTHIKKKK <karthikeyapepeti05@gmail.com>
2026-04-29 15:36:04 +0530

    Syntax,Output,Comments

--- Day-01/Program.cs
+++ Day-01/Program.cs
@@ -6,6 +6,11 @@ namespace Day_01
     {
     static void Main(string[] args)
         {
+            //comments and syntax in c#
+            /*
+             * Multi-line comment
+             * hows C# works
+             */
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Welcome to C# 12.0 and .NET 8.0!");
         }
